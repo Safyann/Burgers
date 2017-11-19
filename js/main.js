cf30a13e5f90abfb4237b88f710a651eecb7a0ff
Alena Safyannikova <alena_safyannik@mail.ru>
2017-11-19 14:33:14 +0500

    Добавлена модалка, карта и слайдер

--- js/main.js
+++ js/main.js
@@ -1,15 +1,113 @@
-var hamburger = document.querySelector(".hamburger-menu-link");
-var hamburgerMenu = document.getElementById('hamburger-menu')
+$(document).ready(() => {
 
-hamburger.addEventListener("click", function() {
-	if (hamburger.classList.contains("hamburger_active")) {
-  	hamburger.classList.remove("hamburger_active");
-  } else {
-  	hamburger.classList.add("hamburger_active");
-  }
-  if (hamburgerMenu.classList.contains("hamburger-menu_active")) {
-  	hamburgerMenu.classList.remove("hamburger-menu_active");
-  } else {
-  	hamburgerMenu.classList.add("hamburger-menu_active");
-  }
+	const hamburger = $(".hamburger-menu-link")
+	const hamburgerMenu = $('#hamburger-menu')
+
+	hamburger.on("click", () => {
+
+		if (hamburger.hasClass('hamburger_active')) {
+      hamburger.removeClass('hamburger_active')
+    } else {
+      hamburger.addClass('hamburger_active')
+    }
+	  if (hamburgerMenu.hasClass("hamburger-menu_active")) {
+	  	hamburgerMenu.removeClass("hamburger-menu_active")
+	  } else {
+	  	hamburgerMenu.addClass("hamburger-menu_active")
+	  }
+	})
+})
+
+$(document).ready(() => {
+
+   $("[data-fancybox]").fancybox({
+  });
+})
+
+$(document).ready(() => {
+
+	$(".maincontent").onepage_scroll({
+   sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
+   easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
+                                    // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
+   animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
+   pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
+   updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+   beforeMove: function(index) {},  // This option accepts a callback function. The function will be called before the page moves.
+   afterMove: function(index) {},   // This option accepts a callback function. The function will be called after the page moves.
+   loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
+   keyboard: true,                  // You can activate the keyboard controls
+   responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
+                                    // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
+                                    // the browser's width is less than 600, the fallback will kick in.
+   direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".
 });
+})
+
+$(document).ready(() => {
+  $(".owl-carousel").owlCarousel({
+		loop: true,
+		items: 1,
+		nav: true,
+		navText: ["",""],
+		navContainer: $(".burger-slider__controls")
+	})
+	$(".burger-slider__btn_next").on("click", e => {
+		e.preventDefault()
+
+		$(".burgers-slider").trigger('next.owl.carousel')
+	})
+	$(".burger-slider__btn_prew").on("click", e => {
+		e.preventDefault()
+
+		$(".burgers-slider").trigger('prew.owl.carousel')
+	})
+});
+
+$(document).ready(() => {
+  $('.team-accordeon').asAccordion({
+    namespace: 'team-accordeon'
+  });
+});
+
+ymaps.ready(init);
+  var myMap
+	var myPlacemark
+
+	function init(){
+    myMap = new ymaps.Map("map", {
+			center: [59.94203885, 30.31515229],
+			zoom: 11
+    });
+		myMap.behaviors.disable('scrollZoom');
+
+        myPlacemarkOne = new ymaps.Placemark([59.94583810, 30.38567458], {
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/icons/map-marker.svg'
+        })
+
+				myPlacemarkTwo = new ymaps.Placemark([59.97510474, 30.30877029], {
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/icons/map-marker.svg'
+        })
+
+				myPlacemarkThree  = new ymaps.Placemark([59.89906607, 30.32578752], {
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/icons/map-marker.svg'
+        })
+
+				myPlacemarkFour  = new ymaps.Placemark([59.90510183, 30.49140672], {
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/icons/map-marker.svg'
+        })
+
+    myMap.geoObjects
+        .add(myPlacemarkOne)
+				.add(myPlacemarkTwo)
+				.add(myPlacemarkThree)
+				.add(myPlacemarkFour)
+  }
